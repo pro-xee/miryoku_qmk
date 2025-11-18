1014a7a5eae142a333031efd8b4da322b5ce3e52
Sebastian Wenninger <sebastian.wenninger@gmail.com>
2025-11-18 10:56:20 +0100

    feat: add pro-xee usespace

---
+++ users/pro-xee/custom_rules.mk
@@ -0,0 +1,9 @@
+# Copyright 2019 Manna Harbour
+# https://github.com/manna-harbour/miryoku
+
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
